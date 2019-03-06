f50e73a0c5f575feda6caca6070c0afe7c9ba385
ZoraGK <jyh199759@126.com>
2019-03-06 16:52:14 +0800

    修改表格式

---
+++ SQL file/Create.sql
@@ -0,0 +1,24 @@
+create table UserLogin
+(
+  Account varchar(8) primary key ,
+  Password varchar(11) not null
+)
+create table TextList
+(
+  TextID  int identity(5000,1) primary key ,
+  TextTitle nvarchar(40) not null,
+  TextTag1 varchar(12),
+  TextTag2 varchar(12),
+  TextTag3 varchar(12),
+  Account varchar(8) not null foreign key references UserLogin(Account),
+  Text nvarchar(4000) not null,
+  TextChangeDate datetime not null default getdate()
+)
+create table CommitList
+(
+  TextID int not null foreign key references TextList(TextID),
+  CommitID int identity(10000,1) primary key ,
+  Account varchar(8) not null foreign key references UserLogin(Account),
+  CommitText nvarchar(100) not null ,
+  CommitChangeDate datetime not null default getdate()
+)
