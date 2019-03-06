f748b19cd6f1e68fadc29a19cf1999edef72fbdf
ZoraGK <jyh199759@126.com>
2019-03-06 11:19:43 +0800

    加法计算器

---
+++ Blog-ASP.NET/Models/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog_ASP.NET.Models
+{
+    public class Calculator
+    {
+        public int GetPlus(int AddA,int AddB)
+        {
+            return AddA + AddB;
+        }
+    }
+}
