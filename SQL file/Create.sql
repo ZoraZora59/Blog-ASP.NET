c7fc6b1c5366a0bdcd6bf5c05c5b6a509660902e
ZoraGK <jyh199759@126.com>
2019-03-07 10:48:40 +0800

    数据库表调整
    去除了TagList表

--- SQL file/Create.sql
+++ SQL file/Create.sql
@@ -7,17 +7,11 @@ create table TextList
 (
   TextID  int identity(5000,1) primary key ,
   TextTitle nvarchar(40) not null,
-  Tag varchar(12) not null foreign key references TagList(Tag),
+  Tag varchar(12) not null,
   Account varchar(8) not null foreign key references UserLogin(Account),
   Text nvarchar(4000) not null,
   Hot int not null default 0,
   TextChangeDate datetime not null default getdate()
-)
-create table TagList
-(
-  Tag varchar(12) not null primary key ,
-  Hot int not null default 0,
-  TextID int not null foreign key references TextList(TextID)
 )
 create table CommitList
 (
@@ -27,6 +21,3 @@ create table CommitList
   CommitText nvarchar(100) not null ,
   CommitChangeDate datetime not null default getdate()
 )
-ALTER TABLE TextList
-ADD FOREIGN KEY (Tag)
-REFERENCES TagList(Tag)
