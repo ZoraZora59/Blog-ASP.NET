2dffb29a21576fd37ea2c20a3f653939439249de
ZoraGK <jyh199759@126.com>
2019-03-07 13:55:24 +0800

    CodeFirst数据库更新

--- Blog-ASP.NET/Global.asax.cs
+++ Blog-ASP.NET/Global.asax.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-
+using System.Data.Entity.Infrastructure;
 namespace Blog_ASP.NET
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer<Models.Blog_ASPNETContext>(new System.Data.Entity. CreateDatabaseIfNotExists<Models.Blog_ASPNETContext>());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
