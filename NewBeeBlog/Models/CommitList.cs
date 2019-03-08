a5e95b9968fa03d19f0367189ceac7af40de4be2
ZoraGK <jyh199759@126.com>
2019-03-08 15:59:57 +0800

    数据表设计

---
+++ NewBeeBlog/Models/CommitList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Blog_ASP.NET.Models
+{
+    public class CommitList
+    {
+        [Key]
+        public int CommitID { get; set; }//评论唯一标识
+
+        [Required]
+        public int TextID { get; set; }//评论所在文章
+        [Required]
+        [MaxLength(8)]
+        public string Account { get; set; }//发布人
+
+        [Required]
+        [MaxLength(100)]
+        public string CommitText { get; set; }//内容
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CommitChangeDate { get; set; }//更新日期
+
+        public ICollection<TextList> Textlists { get; set; }
+    }
+}
---
+++ NewBeeBlog/Models/TextList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Blog_ASP.NET.Models
+{
+    public class TextList
+    {
+        [Key]
+        public int TextID { get; set; }//文章唯一标识
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(40)]
+        public string TextTitle { get; set; }//标题
+        [Required]
+        [MaxLength(20000)]
+        public string Text { get; set; }//内容
+        [Required]
+        public int Hot { get; set; }//点击量
+
+        public string Attachment { get; set; }//附件地址
+        [MaxLength(12)]
+        public string CategoryName { get; set; }//分类
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(8)]
+        public string Account { get; set; }//发布人
+
+        public DateTime TextChangeDate { get; set; }//更新时间
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+
+        //关联表
+        public ICollection<User> Users { get; set; }
+        public ICollection<CommitList> CommitLists { get; set; }
+    }
+}
