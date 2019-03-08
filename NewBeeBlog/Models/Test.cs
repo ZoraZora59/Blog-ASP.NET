3b8fc1587faa0794fc03517706984f75c2a82ea8
ZoraGK <jyh199759@126.com>
2019-03-08 14:44:56 +0800

    测试冲突

---
+++ NewBeeBlog/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoBlog.Models
+{
+    public class Test
+    {
+        public int TestMethod()
+        {
+            int a = 5;
+            int b = 6;
+            return a + b;
+        }
+    }
+}
