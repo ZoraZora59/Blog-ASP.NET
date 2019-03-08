0f8dc12d966829412d3bd01b39f3b00fa8cb6abe
ZoraGK <jyh199759@126.com>
2019-03-08 16:23:48 +0800

    赞还是踩

---
+++ NewBeeBlog/Models/LikeOrDis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewBeeBlog.Models
+{
+    public class LikeOrDis
+    {
+        //文章ID和用户名共同用于标识唯一的赞或踩
+        [Required]
+        public int TextID { get; set; }
+        [MinLength(4)]
+        [MaxLength(8)]
+        public string Account { get; set; }
+        [Required]
+        public short LoD { get; set; }
+
+        public ICollection<TextList> Textlists { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
