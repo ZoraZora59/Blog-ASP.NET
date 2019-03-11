4ad1b96a05b7f81470b01e6bf2f6030c640236b6
ZoraGK <jyh199759@126.com>
2019-03-11 15:02:18 +0800

    合并冲突
    完成登录与注册功能

--- NewBeeBlog/Controllers/ManageController.cs
+++ NewBeeBlog/Controllers/ManageController.cs
@@ -1,25 +1,117 @@
-﻿using System;
+﻿using NewBeeBlog.App_Code;
+using NewBeeBlog.Models;
+using NewBeeBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace NewBeeBlog.Controllers
 {
     public class ManageController : Controller
     {
+        
         // GET: Manage
         public ActionResult Index()
         {
             return View();
         }
-
+        // GET: Register
+        [HttpGet]
         public ActionResult Register()
         {
             return View();
         }
+
+        // Post: Register
+        [HttpPost]
+        public ActionResult Register(RegisterUser model)
+        {
+            if (ModelState.IsValid)
+            //判断是否验证通过
+            {
+                string sessionValidCode = Session["validatecode"]==null?string.Empty: Session["validatecode"].ToString();
+                if (!model.Code.Equals(sessionValidCode))
+                {
+                    return Content("验证码输入错误");
+                }
+                var user = new User();
+                user.Account = model.Account;
+                user.Password = md5tool.GetMD5(model.Password);//需要md5加密否则是明文传输
+                int res = 0;
+                using (NewBeeBlogContext dbContent = new NewBeeBlogContext())
+                //防止并发错误
+
+                {
+                    dbContent.Users.Add(user);
+                    res = dbContent.SaveChanges();
+                    //保存数据库
+                    //SaveChanges返回的是一个int，大于0则正确直接调转到首页
+                }
+                if (res > 0)
+                {
+                    return Redirect("/");
+                }
+                else
+                {
+                    return Content("注册失败");
+                }
+                               
+            }
+            else
+            {
+                return Content("验证失败");
+            }
+        
+
+
+        }
+        // GET: Manage
+        [HttpGet]
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                string sessionValidCode = Session["validatecode"] == null ? string.Empty : Session["validatecode"].ToString();
+                if (!model.Code.Equals(sessionValidCode))
+                {
+                    return Content("验证码输入错误");
+                }
+                var user = new User();
+                user.Account = model.Account;
+                user.Password = md5tool.GetMD5(model.Password);
+                int res = 0;
+                //根据用户名查找实体
+                using (NewBeeBlogContext dbContent = new NewBeeBlogContext())
+                {
+                    var nameUser = dbContent.Users.FirstOrDefault(m => m.Account == model.Account);
+                    if (nameUser == null)
+                    {
+                        return Content("账号或密码不正确");
+                    }
+                    else
+                    {
+                        if (user.Password == nameUser.Password)
+                        {
+                            Session["loginuser"] = nameUser;
+                            return Redirect("/");
+                        }
+                        else
+                        {
+                            return Content("账号或密码不正确");
+                        }
+                    }
+              
+                    
+                }
+            }
             return View();
         }
     }
--- NewBeeBlog/Models/User.cs
+++ NewBeeBlog/Models/User.cs
@@ -9,13 +9,15 @@ namespace NewBeeBlog.Models
     public class User
     {
         //写入数据库的内容包括用户名和密码，CheckPassword仅用于校验，不写入库
-        [Key]
-        [MinLength(4)]
-        [MaxLength(8)]
+
+        public int Id { set; get; }
+        //id自动作为主键
+
+        [MinLength(6)]
+        [MaxLength(16)]
         public string Account { get; set; }
         [Required]
-        [MinLength(6)]
-        [MaxLength(11)]
+        [MaxLength(64)]
         public string Password { get; set; }
 
         [NotMapped]
