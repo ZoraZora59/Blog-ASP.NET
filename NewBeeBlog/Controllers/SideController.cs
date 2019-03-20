261c8d03e72d1938d3ffab9d3b1e9ab817d7ee1d
leno <hzj.boxes@gmail.com>
2019-03-20 16:07:44 +0800

    热度排行

--- NewBeeBlog/Controllers/SideController.cs
+++ NewBeeBlog/Controllers/SideController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewBeeBlog.App_Code;
 using NewBeeBlog.Models;
+using NewBeeBlog.ViewModels;
 
 namespace NewBeeBlog.Controllers
 {
@@ -11,19 +13,46 @@ namespace NewBeeBlog.Controllers
     public class SideController : Controller
     {
         private NewBeeBlogContext db = new NewBeeBlogContext();
+
         [ChildActionOnly]
         public ActionResult Sidebar()
         {
+            var model = new SerializeTool().DeSerialize<BlogConfig>();
+            ViewBag.Config = model;
 
+            var hots = new List<TextListsHot>();
             var blog = from m in db.TextLists
                        select m;
+            //热度排序
+            var list = blog.OrderByDescending(m => m.Hot).Take(5).ToList();
+            foreach(var item in list)
+            {
+                var temp = new TextListsHot();
+                temp.TextID = item.TextID;
+                temp.TextTitle = item.TextTitle;
+                temp.Hot = item.Hot;
+                temp.Datemouth = item.TextChangeDate.ToString().Substring(0,6);
+                hots.Add(temp);
+            }
 
-            var model = blog.OrderByDescending(m => m.Hot).ToList();
+            //时间排序
+            var time_lists = new List<TextListsHot>();
+            var time_list = blog.OrderByDescending(m => m.TextChangeDate).ToList();
+            foreach(var item in time_list)
+            {
+                var temp = new TextListsHot();
+                temp.TextID = item.TextID;
+                temp.TextTitle = item.TextTitle;
+                temp.Hot = item.Hot;
+                temp.Datemouth = item.TextChangeDate.ToString().Substring(0, 6);
+                time_lists.Add(temp);
+            }
+            ViewBag.timesort = time_lists;
 
-            return View("~/Views/Shared/_Sidebar.cshtml", model);
+
+           
+            return View("~/Views/Shared/_Sidebar.cshtml", hots);
             
-
-
         }
         protected override void Dispose(bool disposing)//数据连接释放
         {
---
+++ NewBeeBlog/ViewModels/TextListsHot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewBeeBlog.ViewModels
+{
+    public class TextListsHot
+    {
+        [Key]
+        public int TextID { get; set; }//文章唯一标识
+
+        [Required]
+        [DisplayName("文章标题")]
+        [MaxLength(40)]
+        public string TextTitle { get; set; }//标题
+        
+
+        [DisplayName("点击量")]
+        public int Hot { get; set; }//点击量
+
+        public string Datemouth { get; set; }//月份（字符串）
+    }
+}
