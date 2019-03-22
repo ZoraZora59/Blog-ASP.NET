4dcaeae7769b9408d374e3ba66ed5c52783e1ff0
leno <hzj.boxes@gmail.com>
2019-03-22 10:13:57 +0800

    博文

--- NewBeeBlog/Controllers/HomeController.cs
+++ NewBeeBlog/Controllers/HomeController.cs
@@ -34,8 +34,10 @@ namespace NewBeeBlog.Controllers
             var currentLoginUser = Session["loginuser"] == null ? null : (User)Session["loginuser"];
             ViewBag.currentLoginInfo = currentLoginUser;
 			var models = new List<TextIndex>();
-			var TextList = new List<TextList>();
-			TextList = db.TextLists.ToList();
+            var blog = from m in db.TextLists
+                       select m;
+            var TextList = new List<TextList>();
+            TextList = blog.ToList();
 			foreach(var item in TextList)
 			{
 				var temp = new TextIndex
