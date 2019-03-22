495e9414b345de8ab717d5e7a838738c9894125e
ZoraGK <jyh199759@126.com>
2019-03-22 14:50:48 +0800

    Update SideController.cs

--- NewBeeBlog/Controllers/SideController.cs
+++ NewBeeBlog/Controllers/SideController.cs
@@ -67,7 +67,7 @@ namespace NewBeeBlog.Controllers
                 }
                 temp.Datemouth = item.TextChangeDate.ToString().Substring(0, 6);
             }
-            ViewBag.TopComList = GetTopCmtLst(3);//设定评论排行榜的文章数量
+            ViewBag.TopComList = GetTopCmtLst(5);//设定评论排行榜的文章数量
             ViewBag.categroyList = templist;
             
             //最新评论
@@ -96,7 +96,7 @@ namespace NewBeeBlog.Controllers
             
         }
 
-		public List<TopCmtLst> GetTopCmtLst(int n)
+		public List<TopCmtLst> GetTopCmtLst(int n)//获取评论排行榜
 		{
 			var lTxt = db.TextLists.ToList();
 			if (lTxt.Count == 0)
