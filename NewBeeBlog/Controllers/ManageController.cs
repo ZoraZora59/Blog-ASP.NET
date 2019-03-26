fb5a58e0b8d6b816a9a76e3f4fe3c88e89c7b8eb
ZoraGK <jyh199759@126.com>
2019-03-26 11:06:13 +0800

    后台管理抽层完成

--- NewBeeBlog/Controllers/ManageController.cs
+++ NewBeeBlog/Controllers/ManageController.cs
@@ -21,21 +21,19 @@ namespace NewBeeBlog.Controllers
 	[HandleError]
 	public class ManageController : Controller
 	{
-		private NewBeeBlogContext db = new NewBeeBlogContext();
-
-		public ActionResult Index()//主界面									分层完成
+		public ActionResult Index()//主界面
 		{
 			return View(new GetManage().Mmain);//可能需要异常处理
 		}
 
 		[HttpGet]
-		public ActionResult ManageUser()            //用户管理				分层完成
+		public ActionResult ManageUser()//用户管理
 		{
 			return View();
 		}
 
 		[HttpGet]
-		public ActionResult Update()//文章更新								分层完成
+		public ActionResult Update()//文章更新
 		{
 			try
 			{
@@ -44,7 +42,7 @@ namespace NewBeeBlog.Controllers
 				{
 					int tID = int.Parse(jstID);
 					ViewBag.title = "文章更新";
-					return View(new GetText(tID).Utext);
+					return View(new GetTextInUpdate(tID).Utext);
 				}
 			}
 			catch (NullReferenceException)
@@ -59,18 +57,18 @@ namespace NewBeeBlog.Controllers
 		}
 
 		[HttpGet]
-		public ActionResult ManageCommit()      //评论管理					分层完成
+		public ActionResult ManageCommit()//评论管理
 		{
 			return View();
 		}
 
-		public JsonResult LoadCommit()//加载评论管理界面的数据					分层完成
+		public JsonResult LoadCommit()//加载评论管理界面的数据
 		{
-			return Json(new GetCommitManage().SCommits);
+			return Json(new GetCommitList().SCommits);
 		}
 
 		[HttpGet]
-		public ActionResult RenameCategory()//分类更名						分层完成
+		public ActionResult RenameCategory()//分类更名
 		{
 			try
 			{
@@ -84,7 +82,7 @@ namespace NewBeeBlog.Controllers
 			return View();
 		}
 
-		public JsonResult JSRenameCategory()//分类更名的JS实现				分层完成
+		public JsonResult JSRenameCategory()//分类更名的JS实现
 		{
 			//TODO:修改返回值
 			var NameString = Request["NameChanging"].ToString();
@@ -93,13 +91,13 @@ namespace NewBeeBlog.Controllers
 		}
 
 		[HttpGet]
-		public ActionResult CategoryList()//分类管理							分层完成
+		public ActionResult CategoryList()//分类管理
 		{
-			return View(new GetCategory().mod);
+			return View();
 		}
 
 		[HttpPost]
-		public JsonResult DeleteCategory()//删除指定分类						分层完成
+		public JsonResult DeleteCategory()//删除指定分类
 		{
 			try
 			{
@@ -113,7 +111,7 @@ namespace NewBeeBlog.Controllers
 		}
 
 		[HttpGet]
-		public ActionResult CategoryDetail()//分类详情						分层完成
+		public ActionResult CategoryDetail()//分类详情
 		{
 			try
 			{
@@ -132,7 +130,7 @@ namespace NewBeeBlog.Controllers
 		}
 
         [HttpPost]
-        public JsonResult DeleteText()//文章删除								分层完成
+        public JsonResult DeleteText()//文章删除
         {
             try
             {
@@ -150,25 +148,25 @@ namespace NewBeeBlog.Controllers
 			//TODO:JS在回复成功后应当删除对应标签
         }
         
-        public JsonResult LoadTextList()//文章管理列表的JS实现				分层完成
+        public JsonResult LoadTextList()//文章管理列表的JS实现
         {
             return Json(new GetTextList().ManageTexts);
         }
 
         [HttpGet]
-        public ActionResult TextList()//博文管理的列表页						分层完成
+        public ActionResult TextList()//博文管理的列表页
         {
             return View(new GetTextList().ManageTexts);
         }
 
         [HttpGet]
-        public ActionResult Register()//注册的页面显示						分层完成
+        public ActionResult Register()//注册的页面显示
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Register(RegisterUser model)//注册信息提交		分层完成
+        public ActionResult Register(RegisterUser model)//注册信息提交
 		{
             if (ModelState.IsValid)
             //判断是否验证通过
@@ -190,202 +188,110 @@ namespace NewBeeBlog.Controllers
         }
 
         [HttpGet]
-        public ActionResult Login()//登录的页面显示							分层完成
+        public ActionResult Login()//登录的页面显示
 		{
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(LoginUser model)//登录信息提交				分层完成
+        public ActionResult Login(LoginUser model)//登录信息提交
 		{
-            if (ModelState.IsValid)
-            {
-                string sessionValidCode = Session["validatecode"] == null ? string.Empty : Session["validatecode"].ToString();
-                if (!model.Code.Equals(sessionValidCode))
-                {
-                    return Content("验证码输入错误");
-                }
-                var user = new User
-                {
-                    Account = model.Account,
-                    Password = md5tool.GetMD5(model.Password)
-                };
-                //根据用户名查找实体
-                using (NewBeeBlogContext dbContent = new NewBeeBlogContext())
-                {
-                    var nameUser = dbContent.Users.FirstOrDefault(m => m.Account == model.Account);
-                    if (nameUser == null)
-                    {
-                        return Content("账号或密码不正确");
-                    }
-                    else
-                    {
-                        if (user.Password == nameUser.Password)
-                        {
-                            Session["loginuser"] = nameUser;
-                            return Redirect("/");
-                        }
-                        else
-                        {
-                            return Content("账号或密码不正确");
-                        }
-                    }
-                }
+			if (ModelState.IsValid)
+			{
+				string sessionValidCode = Session["validatecode"] == null ? string.Empty : Session["validatecode"].ToString();
+				if (!model.Code.Equals(sessionValidCode))
+				{
+					return Content("验证码输入错误，请返回上一页重试");
+				}
+				var isLog = new Login(model);
+				if (isLog.IsLogin == false)
+				{
+					return Content("账号或密码不正确，请返回上一页重试");
+				}
+				else
+				{
+					Session["loginuser"] = isLog.LoginData;
+					return Redirect("/");
+				}
             }
             return View();
         }
 
         [HttpGet]
-        public ActionResult Config()
-        {
+        public ActionResult Config()//读取博客配置XML文件
+		{
             var model = new SerializeTool().DeSerialize<BlogConfig>();
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult Config(BlogConfig model)
-        {
+        public ActionResult Config(BlogConfig model)//设定博客配置文件
+		{
             new SerializeTool().Serialize<BlogConfig>(model);
             return View();
-
         }
 
         [HttpGet]
-        public ActionResult AddCategroy()
+        public ActionResult AddCategroy()//添加分类的页面显示
         {
             return View();
         }
 
-        public JsonResult LoadCategoryList()
+        public JsonResult LoadCategoryList()//加载分类表
         {
-            
-            List<TextList> temp = new List<TextList>();
-            List<CategoryList> CategoryLists = new List<CategoryList>();
-            temp = db.TextLists.ToList();
-            foreach (var item in temp)
-            {
-				var categoryItem = new CategoryList
-				{
-					CategoryName = item.CategoryName
-				};//这里的categoryItem应当在每次循环时new，以避免重复对同一项进行修改
-				if (categoryItem.CategoryName == null)
-                {
-                    categoryItem.CategoryName = "未分类";
-                }
-                if (!CategoryLists.Exists(T => T.CategoryName == categoryItem.CategoryName))//若不存在于已生成列表则添加进列表
-                {
-                    categoryItem.CategoryHot += item.Hot;
-                    categoryItem.TextCount++;
-                    CategoryLists.Add(categoryItem);
-                }
-                else//否则对指定项进行修改
-                {
-                    CategoryLists.Find(CategoryList => CategoryList.CategoryName == categoryItem.CategoryName).CategoryHot += item.Hot;
-                    CategoryLists.Find(CategoryList => CategoryList.CategoryName == categoryItem.CategoryName).TextCount++;
-                }
-            }
-            return Json(CategoryLists);
+            return Json(new GetCategoryList().mod);
         }
 
         [HttpGet]
-        public ActionResult ManageUsers()
-        {
-            return View();
-        }
-
-        public JsonResult LoadUsers()
-        {
-			List<ManageUser> manageUsers = new List<ManageUser>();
-			var trans = db.Users.Select(m => new { m.Account, m.Name }).ToList();
-			trans.Remove(trans.Find(a => a.Account == "admin123"));
-			foreach (var item in trans)
-			{
-				ManageUser temp = new ManageUser
-				{
-					Account = item.Account,
-					Name = item.Name,
-					CommitCount = 0
-				};
-				var cmtlist = db.CommitLists.Where(cmt => cmt.Account == temp.Account);
-				foreach (var cmt in cmtlist)
-				{
-					temp.CommitCount++;
-				}
-				manageUsers.Add(temp);
-			}
-			return Json(manageUsers);
-        }
-
-        public ActionResult DelUsers(string Account)
-        {
-            var odlModel = db.Users.Find(Account);
-            if (odlModel == null)
-            {
-                return HttpNotFound();
-            }
-			while(db.CommitLists.Where(c=>c.Account==Account).FirstOrDefault()!=null)//同时删除用户的评论
-			{
-				db.CommitLists.Remove(db.CommitLists.Where(c => c.Account == Account).FirstOrDefault());
-				db.SaveChanges();
-			}
-            db.Users.Remove(odlModel);
-            db.SaveChanges();
-            return Content("删除成功");
-        }
-
-		
-		public string GetFirstView(string Content)//截取文章预览片段
+        public ActionResult ManageUsers()//用户管理的页面显示
 		{
-			Content=Regex.Replace(Content, "<[^>]+>", "");
-			Content = Regex.Replace(Content, "&[^;]+;", "");
-			if (Content.Length < 105)
-				return Content;
-			else
-				Content = Content.Substring(0,100);
-			return Content;
-		}
+            return View();
+        }
 
+        public JsonResult LoadUsers()//加载用户表	
+        {
+			return Json(new GetUserList().ManageUsers);
+        }
+
+        public ActionResult DelUsers(string Account)//删除用户 TODO:JS化
+        {
+			try
+			{
+				var IsDel = new DelUser(Account);
+				if(IsDel.IsSuccess==false)
+				{
+					return HttpNotFound();//删除失败的反馈
+				}
+			}
+			catch (Exception)
+			{
+				//TODO:删除失败的反馈
+				throw;
+			}
+            return Content("删除成功,请刷新页面");//页面更新
+        }
+		
 		[HttpPost]
         [ValidateInput(false)]
         public ActionResult Update([Bind(Include =
-			"Id,Title,Category,Text")] UpdateText BlogText)
+			"Id,Title,Category,Text")] UpdateText BlogText)//文章更新提交
         {
             if (ModelState.IsValid)
             {
-				if(BlogText.Id!=0)
+				try
 				{
-					if (db.TextLists.FirstOrDefault(c => c.TextID == BlogText.Id) != null)
+					var IsUpdate = new UpdateBlog(BlogText);
+					if (IsUpdate.IsSuccess == true)//TODO:更新成功处理
 					{
-						//1.先查询要修改的原数据
-						TextList modelNew = db.TextLists.Where(a => a.TextID == BlogText.Id).FirstOrDefault();
-						modelNew.FirstView = GetFirstView(BlogText.Text);
-						//2.设置修改后的值
-						modelNew.TextTitle = BlogText.Title;
-						modelNew.CategoryName = BlogText.Category;
-						modelNew.Text = BlogText.Text;
-						db.SaveChanges();
-					}
-					else
-					{
-						return Content("更新失败，请确认需要更新的文章是否存在。如需重试请刷新页面......");
+						return Redirect("/manage/textlist");
 					}
 				}
-				else
+				catch (Exception)
 				{
-					try
-					{
-						db.TextLists.Add(new TextList { TextTitle = BlogText.Title, CategoryName = BlogText.Category, Text = BlogText.Text, FirstView=GetFirstView(BlogText.Text) });
-						db.SaveChanges();
-					}
-					catch (Exception)//TODO:添加异常处理信息
-					{
-
-						throw;
-					}
-				}
+					throw;
+				} 
             }
-
-            return Redirect("/manage/textlist");
+            return Redirect("/manage/textlist");//TODO:更新失败处理
         }
 
 		[HttpGet]
@@ -393,14 +299,10 @@ namespace NewBeeBlog.Controllers
 		{
 			try
 			{
-				string jstID = Request["TextID"].ToString();
-				if (jstID != null)
+				var IsGetText = new GetTextInManageDetail(int.Parse(Request["TextID"].ToString()));
+				if (IsGetText.IsSuccess == true)
 				{
-					int tid = int.Parse(jstID);
-					TextList model = new TextList();
-					model = db.TextLists.Find(tid);
-					if (model != null)
-						return View(model);
+					return View(IsGetText.Text);
 				}
 			}
 			catch (Exception)
@@ -411,12 +313,13 @@ namespace NewBeeBlog.Controllers
 			return Redirect("/Manage/TextList");
 		}
 
+		#region Kindeditor上传
 		public ActionResult Upload()
-        {
-            //文件保存目录路径
-            String savePath = "/attached/";
-            //文件保存目录URL
-            String saveUrl = "/attached/";
+		{
+			//文件保存目录路径
+			String savePath = "/attached/";
+			//文件保存目录URL
+			String saveUrl = "/attached/";
 			//定义允许上传的文件扩展名
 			Hashtable extTable = new Hashtable
 			{
@@ -427,71 +330,63 @@ namespace NewBeeBlog.Controllers
 			};
 			//最大文件大小
 			int maxSize = 1000000;
-            HttpPostedFileBase imgFile = Request.Files["imgFile"];
-            if (imgFile == null)
-            {
-                return Content("请选择文件。");
-            }
-            String dirPath = Server.MapPath(savePath);
-            if (!Directory.Exists(dirPath))
-            {
-                return Content("上传目录不存在。");
-            }
-            String dirName = Request.QueryString["dir"];
-            if (String.IsNullOrEmpty(dirName))
-            {
-                dirName = "image";
-            }
-            if (!extTable.ContainsKey(dirName))
-            {
-                return Content("目录名不正确。");
-            }
-            String fileName = imgFile.FileName;
-            String fileExt = Path.GetExtension(fileName).ToLower();
-            if (imgFile.InputStream == null || imgFile.InputStream.Length > maxSize)
-            {
-                return Content("上传文件大小超过限制。");
-            }
-            if (String.IsNullOrEmpty(fileExt) || Array.IndexOf(((String)extTable[dirName]).Split(','), fileExt.Substring(1).ToLower()) == -1)
-            {
-                return Content("上传文件扩展名是不允许的扩展名。\n只允许" + ((String)extTable[dirName]) + "格式。");
-            }
-            //创建文件夹
-            dirPath += dirName + "/";
-            saveUrl += dirName + "/";
-            if (!Directory.Exists(dirPath))
-            {
-                Directory.CreateDirectory(dirPath);
-            }
-            String ymd = DateTime.Now.ToString("yyyyMMdd", DateTimeFormatInfo.InvariantInfo);
-            dirPath += ymd + "/";
-            saveUrl += ymd + "/";
-            if (!Directory.Exists(dirPath))
-            {
-                Directory.CreateDirectory(dirPath);
-            }
-            String newFileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff", DateTimeFormatInfo.InvariantInfo) + fileExt;
-            String filePath = dirPath + newFileName;
-            imgFile.SaveAs(filePath);
-            String fileUrl = saveUrl + newFileName;
+			HttpPostedFileBase imgFile = Request.Files["imgFile"];
+			if (imgFile == null)
+			{
+				return Content("请选择文件。");
+			}
+			String dirPath = Server.MapPath(savePath);
+			if (!Directory.Exists(dirPath))
+			{
+				return Content("上传目录不存在。");
+			}
+			String dirName = Request.QueryString["dir"];
+			if (String.IsNullOrEmpty(dirName))
+			{
+				dirName = "image";
+			}
+			if (!extTable.ContainsKey(dirName))
+			{
+				return Content("目录名不正确。");
+			}
+			String fileName = imgFile.FileName;
+			String fileExt = Path.GetExtension(fileName).ToLower();
+			if (imgFile.InputStream == null || imgFile.InputStream.Length > maxSize)
+			{
+				return Content("上传文件大小超过限制。");
+			}
+			if (String.IsNullOrEmpty(fileExt) || Array.IndexOf(((String)extTable[dirName]).Split(','), fileExt.Substring(1).ToLower()) == -1)
+			{
+				return Content("上传文件扩展名是不允许的扩展名。\n只允许" + ((String)extTable[dirName]) + "格式。");
+			}
+			//创建文件夹
+			dirPath += dirName + "/";
+			saveUrl += dirName + "/";
+			if (!Directory.Exists(dirPath))
+			{
+				Directory.CreateDirectory(dirPath);
+			}
+			String ymd = DateTime.Now.ToString("yyyyMMdd", DateTimeFormatInfo.InvariantInfo);
+			dirPath += ymd + "/";
+			saveUrl += ymd + "/";
+			if (!Directory.Exists(dirPath))
+			{
+				Directory.CreateDirectory(dirPath);
+			}
+			String newFileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff", DateTimeFormatInfo.InvariantInfo) + fileExt;
+			String filePath = dirPath + newFileName;
+			imgFile.SaveAs(filePath);
+			String fileUrl = saveUrl + newFileName;
 			Hashtable hash = new Hashtable
 			{
 				["error"] = 0,
 				["url"] = fileUrl
 			};
 			return Json(hash);
-        }
+		}
+		#endregion
 
 		#region 全局方法
-		protected override void Dispose(bool disposing)//数据连接释放
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
 		protected override void HandleUnknownAction(string actionName)//自定义404ERROR
 		{
 			Response.Redirect("/home");
